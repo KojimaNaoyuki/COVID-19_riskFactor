b3eb335867b9f274f7806d3c9b7a77897dcbc390
KojimaNaoyuki <programkoji213@gmail.com>
2021-02-24 15:22:46 +0900

    2021/02/24/5

--- scripts/data/data_population.js
+++ scripts/data/data_population.js
@@ -1,6 +1,6 @@
 class Data {
     constructor() {
-        this.dataPopulation = {toukyou: 13960236, saitama: 7342684, kanagawa: 9216009};
+        this.dataPopulation = {toukyou: 13960236, saitama: 7342684, kanagawa: 9216009, tiba: 6281869, gunnma: 1924116, totigi: 1942313, ibaraki: 283172, yamanasi: 829884};
     }
     
     getData() {

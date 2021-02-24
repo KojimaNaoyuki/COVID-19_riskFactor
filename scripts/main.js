ba2929dbf935b91936c3db3f78a273876c03d663
KojimaNaoyuki <programkoji213@gmail.com>
2021-02-24 15:11:10 +0900

    2021/02/24/4

--- scripts/main.js
+++ scripts/main.js
@@ -10,11 +10,17 @@ class Main {
         this.infectionRateCal = new InfectionRateCal();
 
         this.DOM.btn = document.querySelector('#decision-btn');
+        this.DOM.result = document.querySelector('.result');
         this._addEvent();
+    }
+
+    _arrowAdd() {
+        this.DOM.result.classList.add('calRuned');
     }
 
     _addEvent() {
         this.DOM.btn.addEventListener('click', this.infectionRateCal.cal.bind(this.infectionRateCal));
         this.DOM.btn.addEventListener('click', this.infectionRateCal.displayCal.bind(this.infectionRateCal));
+        this.DOM.btn.addEventListener('click', this._arrowAdd.bind(this));
     }
 }
